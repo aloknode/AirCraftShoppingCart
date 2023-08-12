e360809bee3ea40624220496ce4318278ec2b69c
Alok Maity <alokjucse@gmail.com>
2023-08-12 23:31:48 +0530

    Added my project

---
+++ AirCraftShoppingCart/config/database.js
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+module.exports={
+    database:'mongodb://127.0.0.1:27017/AirCraftShoppingCart'
+};
+
+
